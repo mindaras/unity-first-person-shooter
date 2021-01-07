ac01f0d67204262f82bb057f12247f135eabbb23
Mindaugas Lazauskas <mindaugas.lazauskas@zedge.net>
2021-01-07 22:15:47 +0200

    3D player model, movement forward animations

---
+++ Assets/Player/Scripts/MovementAnimation.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAnimation : MonoBehaviour
+{
+    public Animator animator;
+
+    private int isWalking;
+
+    private int isRunning;
+
+    void Start()
+    {
+        isWalking = Animator.StringToHash("isWalking");
+        isRunning = Animator.StringToHash("isRunning");
+    }
+
+    void Update()
+    {
+        var z = Input.GetAxis("Vertical");
+        var isWalkingSet = animator.GetBool(isWalking);
+        var isRunningSet = animator.GetBool(isRunning);
+
+        if (z > 0)
+        {
+            if (!isWalkingSet)
+            {
+                animator.SetBool(isWalking, true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                animator.SetBool(isRunning, true);
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                animator.SetBool(isRunning, false);
+            }
+        }
+
+        if (z == 0)
+        {
+            if (isWalkingSet)
+            {
+                animator.SetBool(isWalking, false);
+            }
+
+            if (isRunningSet)
+            {
+                animator.SetBool(isRunning, false);
+            }
+        }
+    }
+}
